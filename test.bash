7d2f5fb638d85410187bc8ae3207d45b5309e63f
aidar <a.bariev@infotech.team>
2017-04-12 23:13:55 +0300

    Changed test.bash

--- test.bash
+++ test.bash
@@ -4,10 +4,12 @@
 git reset --hard
 
 # install jar dependency
-cd play24/jar-dependency && mvn clean install
+cd play24/jar-dependency
+mvn clean install
 
 # run play app in detached console
-cd ../main-project && tail -f /dev/null | mvn clean play2:run &
+cd ../main-project
+tail -f /dev/null | mvn clean play2:run &
 
 # wait for 9000 port opening
 while ! nc -z localhost 9000; do   
@@ -15,12 +17,13 @@ while ! nc -z localhost 9000; do
 done
 
 # curl on user-info
-curl http://localhost:9000/current-user | grep admin_42
+curl -s http://localhost:9000/current-user | grep admin_42
 
 # change source file
-
+sed -i "" 's/admin_42/admin_56/g' app/service/UserServiceImpl.java
 
 # wait for 10 seconds
+sleep 10
 
 # curl on user-info once again
-#curl http://localhost:9000/user-info
+curl -s http://localhost:9000/current-user | grep admin_56
